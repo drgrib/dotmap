2b4baa6d8e4250cbd7f2438dabbf8a8ed896ddfb
dgrib <credford@gmail.com>
2015-08-12 09:20:47 -0700

    better setup

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup(
 	author = 'Chris Redford',
 	author_email = 'credford@gmail.com',
 	url = 'https://github.com/drgrib/dotmap', # use the URL to the github repo
-	download_url = 'https://github.com/drgrib/dotmap/1.0',
+	download_url = 'https://github.com/drgrib/dotmap/tarball/1.0',
 	keywords = ['dict', 'dot', 'map', 'order', 'ordered', 'ordereddict', 'access', 'dynamic'], # arbitrary keywords
 	classifiers = [],
-	install_requires=['collections','pprint'],
 )
