c523e6aec88c863855a8635b1c8d86c6f6db46d1
drgrib <chris.redford@weather.com>
2015-08-24 13:44:59 -0700

    proper dict subclassing

--- dotmap/__init__.py
+++ dotmap/__init__.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
 from pprint import pprint
 
-class DotMap(object):
+class DotMap(OrderedDict):
 	def __init__(self, *args, **kwargs):
 		self._map = OrderedDict()
 		if args:
@@ -71,8 +71,62 @@ class DotMap(object):
 	def pprint(self):
 		pprint(self.toDict())
 
+	# proper dict subclassing
 	def values(self):
 		return self._map.values()
+	def __cmp__(self, other):
+		return self._map.__cmp__(other._map)
+	def __eq__(self, other):
+		return self._map.__eq__(other._map)
+	def __ge__(self, other):
+		return self._map.__ge__(other._map)
+	def __gt__(self, other):
+		return self._map.__gt__(other._map)
+	def __le__(self, other):
+		return self._map.__le__(other._map)
+	def __lt__(self, other):
+		return self._map.__lt__(other._map)
+	def __ne__(self, other):
+		return self._map.__ne__(other._map)
+	def __delitem__(self, key):
+		return self._map.__delitem__(key)
+	def __len__(self):
+		return self._map.__len__()
+	def clear(self):
+		self._map.clear()
+	def copy(self):
+		return self
+	def get(self, key, default=None):
+		return self._map.get(key, default)
+	def has_key(self, key):
+		return self._map.has_key(key)
+	def iterkeys(self):
+		return self._map.iterkeys()
+	def itervalues(self):
+		return self._map.itervalues()
+	def keys(self):
+		return self._map.keys()
+	def pop(self, key, default=None):
+		return self._map.pop(key, default)
+	def popitem(self):
+		return self._map.popitem()
+	def setdefault(self, key, default=None):
+		self._map.setdefault(key, default)
+	def update(self, *args, **kwargs):
+		if len(args) != 0:
+			self._map.update(*args)
+		self._map.update(kwargs)
+	def viewitems(self):
+		return self._map.viewitems()
+	def viewkeys(self):
+		return self._map.viewkeys()
+	def viewvalues(self):
+		return self._map.viewvalues()
+	@classmethod
+	def fromkeys(cls, seq, value=None):
+		d = DotMap()
+		d._map = OrderedDict.fromkeys(seq, value)
+		return d
 
 if __name__ == '__main__':
 	d = {
@@ -82,6 +136,19 @@ if __name__ == '__main__':
 	}
 	dd = DotMap(d)
 	print(dd)
+	print(len(dd))
+	print(dd.copy())
+	print(dd)
+	print(OrderedDict.fromkeys([1,2,3]))
+	print(DotMap.fromkeys([1,2,3], 'a'))
+	print(dd.get('a'))
+	print(dd.get('f',33))
+	print(dd.get('f'))
+	print(dd.has_key('a'))
+	dd.update([('rat',5),('bum',4)], dog=7,cat=9)
+	dd.update({'lol':1,'ba':2})
+	print(dd)
+	print
 	for k in dd:
 		print(k)
 	print('a' in dd)
@@ -92,4 +159,5 @@ if __name__ == '__main__':
 	print
 	print(dd.values())
 	dm = DotMap(name='Steve', job='programmer')
-	print dm
+	print(dm)
+	print issubclass(dm.__class__, dict)
