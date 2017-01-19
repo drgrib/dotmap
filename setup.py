b54c48cab9aeecaa260d1b13e89813923396b281
Chris Redford <credford@gmail.com>
2017-01-19 10:56:36 -0800

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-	version = '1.2.14',
+	version = '1.2.15',
 	name = 'dotmap',
 	packages = ['dotmap'], # this must be the same as the name above
 	description = 'ordered, dynamically-expandable dot-access dictionary',
