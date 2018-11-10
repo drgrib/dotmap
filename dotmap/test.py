767d6e39be20e1964c7caeb6be93300e4bf77efc
Chris Redford <credford@gmail.com>
2018-11-10 10:24:55 -0800

    more tests

--- dotmap/test.py
+++ dotmap/test.py
@@ -20,3 +20,20 @@ class BasicTestCase(unittest.TestCase):
         self.assertIsInstance(dm.subD, DotMap)
         self.assertEqual(dm.subD.c, 3)
         self.assertEqual(dm.subD.d, 4)
+
+    def test_copy(self):
+        dm = DotMap(self.d)
+        dm_copy = dm.copy()
+        self.assertIsInstance(dm_copy, DotMap)
+        self.assertEqual(dm_copy.a, 1)
+        self.assertEqual(dm_copy.b, 2)
+        self.assertIsInstance(dm_copy.subD, DotMap)
+        self.assertEqual(dm_copy.subD.c, 3)
+        self.assertEqual(dm_copy.subD.d, 4)
+
+    def test_fromkeys(self):
+        dm = DotMap.fromkeys([1, 2, 3], 'a')
+        self.assertEqual(len(dm), 3)
+        self.assertEqual(dm[1], 'a')
+        self.assertEqual(dm[2], 'a')
+        self.assertEqual(dm[3], 'a')
