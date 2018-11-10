72df026af422c345ca2cfe90d50a23d4064cebca
Chris Redford <credford@gmail.com>
2018-11-10 10:20:19 -0800

    test_dict_init

--- dotmap/test.py
+++ dotmap/test.py
@@ -1,18 +1,22 @@
 import unittest
 from dotmap import DotMap
 
-d = {
-    'a': 1,
-    'b': 2,
-    'subD': {'c': 3, 'd': 4}
-}
-
-dm = DotMap(d)
-
 
 class BasicTestCase(unittest.TestCase):
     '''Tests basic functionality'''
 
-    def test_print(self):
+    def setUp(self):
+        self.d = {
+            'a': 1,
+            'b': 2,
+            'subD': {'c': 3, 'd': 4}
+        }
+
+    def test_dict_init(self):
+        dm = DotMap(self.d)
+        self.assertIsInstance(dm, DotMap)
         self.assertEqual(dm.a, 1)
         self.assertEqual(dm.b, 2)
+        self.assertIsInstance(dm.subD, DotMap)
+        self.assertEqual(dm.subD.c, 3)
+        self.assertEqual(dm.subD.d, 4)
