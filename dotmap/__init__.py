e47925fcb3d0f34eedefecdf7167d07655afd6e6
Eric Poulsen <eric@zyxod.com>
2020-10-11 13:46:48 -0700

    Implementation of _prevent_method_masking (#70)
    * implemented _allow_all_keys
    
    * _allow_all_keys --> _prevent_method_masking, added some simple tests

--- dotmap/__init__.py
+++ dotmap/__init__.py
@@ -16,46 +16,48 @@ def here(item=None):
         out += '({})'.format(item)
     print(out)
 
+__all__ = ['DotMap']
 
 class DotMap(MutableMapping, OrderedDict):
     def __init__(self, *args, **kwargs):
         self._map = OrderedDict()
-        self._dynamic = True
-        if kwargs:
-            if '_dynamic' in kwargs:
-                self._dynamic = kwargs['_dynamic']
+        self._dynamic = kwargs.pop('_dynamic', True)
+        self._prevent_method_masking = kwargs.pop('_prevent_method_masking', False)
+
         if args:
             d = args[0]
             # for recursive assignment handling
             trackedIDs = {id(d): self}
 
-            src = None
+            src = []
             if isinstance(d, MutableMapping):
                 src = self.__call_items(d)
             elif isinstance(d, Iterable):
                 src = d
 
-            if src:
-                for k,v in src:
-                    if isinstance(v, dict):
-                        if id(v) in trackedIDs:
-                            v = trackedIDs[id(v)]
-                        else:
-                            v = self.__class__(v, _dynamic=self._dynamic)
-                            trackedIDs[id(v)] = v
-                    if type(v) is list:
-                        l = []
-                        for i in v:
-                            n = i
-                            if isinstance(i, dict):
-                                n = self.__class__(i, _dynamic=self._dynamic)
-                            l.append(n)
-                        v = l
-                    self._map[k] = v
+            for k,v in src:
+                if self._prevent_method_masking and k in reserved_keys:
+                    raise KeyError('"{}" is reserved'.format(k))
+                if isinstance(v, dict):
+                    if id(v) in trackedIDs:
+                        v = trackedIDs[id(v)]
+                    else:
+                        v = self.__class__(v, _dynamic=self._dynamic, _prevent_method_masking = self._prevent_method_masking)
+                        trackedIDs[id(v)] = v
+                if type(v) is list:
+                    l = []
+                    for i in v:
+                        n = i
+                        if isinstance(i, dict):
+                            n = self.__class__(i, _dynamic=self._dynamic, _prevent_method_masking = self._prevent_method_masking)
+                        l.append(n)
+                    v = l
+                self._map[k] = v
         if kwargs:
             for k,v in self.__call_items(kwargs):
-                if k != '_dynamic':
-                    self._map[k] = v
+                if self._prevent_method_masking and k in reserved_keys:
+                    raise KeyError('"{}" is reserved'.format(k))
+                self._map[k] = v
 
     def __call_items(self, obj):
         if hasattr(obj, 'iteritems') and ismethod(getattr(obj, 'iteritems')):
@@ -84,8 +86,10 @@ class DotMap(MutableMapping, OrderedDict):
         return self._map[k]
 
     def __setattr__(self, k, v):
-        if k in {'_map','_dynamic', '_ipython_canary_method_should_not_exist_'}:
+        if k in {'_map','_dynamic', '_ipython_canary_method_should_not_exist_', '_prevent_method_masking'}:
             super(DotMap, self).__setattr__(k,v)
+        elif self._prevent_method_masking and k in reserved_keys:
+            raise KeyError('"{}" is reserved'.format(k))
         else:
             self[k] = v
 
@@ -328,6 +332,7 @@ class DotMap(MutableMapping, OrderedDict):
         s = '\n'.join(lines)
         return s
 
+reserved_keys = {i for i in dir(DotMap) if not i.startswith('__') and not i.endswith('__')}
 
 if __name__ == '__main__':
     # basics
@@ -602,6 +607,45 @@ if __name__ == '__main__':
     print(p.first.nested)
     p.first.second.third = 456
     print(p.first.second.third)
+
+    print('\n== DotMap method masking ==')
+    # method masking tests
+    d = DotMap(a=1,get='mango')
+    d = DotMap((('a',1),('get','mango')))
+    d = DotMap({'a':1, 'get': 'mango'})
+    d = DotMap({'a':1, 'b': {'get': 'mango'}})
+    d.a = {'get':'mongo'}
+
+    try:
+        d = DotMap(a=1,get='mango', _prevent_method_masking = True)
+        raise RuntimeError("this should fail with KeyError")
+    except KeyError:
+        print('kwargs method masking ok')
+
+    try:
+        d = DotMap((('a',1),('get','mango')), _prevent_method_masking = True)
+        raise RuntimeError("this should fail with KeyError")
+    except KeyError:
+        print('iterable method masking ok')
+
+    try:
+        d = DotMap({'a':1, 'get': 'mango'}, _prevent_method_masking = True)
+        raise RuntimeError("this should fail with KeyError")
+    except KeyError:
+        print('dict method masking ok')
+
+    try:
+        d = DotMap({'a':1, 'b': {'get': 'mango'}}, _prevent_method_masking = True)
+        raise RuntimeError("this should fail with KeyError")
+    except KeyError:
+        print('nested dict method masking ok')
+
+    try:
+        d = DotMap({'a':1, 'b': {}}, _prevent_method_masking = True)
+        d.b.get = 7
+        raise RuntimeError("this should fail with KeyError")
+    except KeyError:
+        print('nested dict attrib masking ok')
 
     # final print
     print()
