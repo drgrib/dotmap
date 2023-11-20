032df78b5f969022af13896a50c992235390bd0b
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2023-11-20 23:08:03 +0100

    migrate all settings from "setup.py" to "setup.cfg" (#102)
    The setup.py mechanism is deprecated in favor of declarative package metadata.

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setup(
-	version = '1.3.30',
-    name='dotmap',
-    packages=['dotmap'],  # this must be the same as the name above
-    description='ordered, dynamically-expandable dot-access dictionary',
-    author='Chris Redford',
-    author_email='credford@gmail.com',
-    url='https://github.com/drgrib/dotmap',  # use the URL to the github repo
-    keywords=['dict', 'dot', 'map', 'order', 'ordered',
-              'ordereddict', 'access', 'dynamic'],  # arbitrary keywords
-    python_requires=">= 3.6",
-    classifiers=[],
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="MIT",
-)
+setup()
